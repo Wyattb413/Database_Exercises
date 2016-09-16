69b0312c4c3b4086f218acf6a6cf9c5d8289602b
Wyatt Barnes <wyattb413@gmail.com>
2016-09-16 16:19:23 -0500

    Practicing Inserting into table

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+INSERT INTO albums (author_first_name, author_last_name, release_date, sales_in_millions, genre) 
+VALUES ('Micheal', 'Jackson', 1982, 46, 'Pop, Rock, R&B'),
+('AC/DC', 'N/A', 1980, 26.1, 'Hard Rock'),
+('Pink Floyd', 'N/A', 1973, 24.2, 'Progressive Rock'),
+('Whitney', 'Houston', 1992, 27.4, 'R&B, Soul, Pop'),
+('Meat Loaf', 'N/A', 1977, 20.6, 'Hard Rock, Progressive Rock'),
+('Eagles', 'N/A', 1976, 32.2, 'Rock, Soft Rock, Folk Rock'),
+('Bee Gees', 'N/A', 1977, 20.6, 'Disco'),
+('Fleetwood Mac', 'N/A', 1977, 27.9, 'Soft Rock');
